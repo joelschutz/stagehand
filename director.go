46aff3825ce5cf88ca68608caa53077d073c1c7b
Joel Schutz <joelsschutz@yahoo.com.br>
2024-05-28 17:02:06 -0300

    Streamline director behaviour

--- director.go
+++ director.go
@@ -25,15 +25,8 @@ func NewSceneDirector[T any](scene Scene[T], state T, RuleSet map[Scene[T]][]Dir
 // ProcessTrigger finds if a transition should be triggered
 func (d *SceneDirector[T]) ProcessTrigger(trigger SceneTransitionTrigger) {
 	if prevTransition, ok := d.current.(SceneTransition[T]); ok {
-		// previous transition is still running, if related to trigger end it to start the next transition
-		for _, directiveList := range d.RuleSet {
-			for _, directive := range directiveList {
-				if directive.Trigger == trigger && prevTransition == directive.Transition {
-					prevTransition.End()
-					return
-				}
-			}
-		}
+		// previous transition is still running, end it to process trigger
+		prevTransition.End()
 	}
 
 	for _, directive := range d.RuleSet[d.current.(Scene[T])] {
